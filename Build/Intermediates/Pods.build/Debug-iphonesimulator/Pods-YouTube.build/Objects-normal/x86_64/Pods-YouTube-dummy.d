b0bcb8d4baf67e63db6d6f46986c45a7cf08b9da
Daniele Bonilha <dani.bonilha@live.com>
2017-10-30 19:09:22 -0300

    Profile

--- Build/Intermediates/Pods.build/Debug-iphonesimulator/Pods-YouTube.build/Objects-normal/x86_64/Pods-YouTube-dummy.d
+++ Build/Intermediates/Pods.build/Debug-iphonesimulator/Pods-YouTube.build/Objects-normal/x86_64/Pods-YouTube-dummy.d
@@ -1,3 +1,3 @@
 dependencies: \
-  /Users/daniele.bonilha/mob/Quick-Chat/youtube-iOS/Pods/Target\ Support\ Files/Pods-YouTube/Pods-YouTube-dummy.m \
-  /Users/daniele.bonilha/mob/Quick-Chat/youtube-iOS/Build/Intermediates/Pods.build/Debug-iphonesimulator/Pods-YouTube.build/module.modulemap
+  /Users/daniele.bonilha/mob/youtube-iOS/Pods/Target\ Support\ Files/Pods-YouTube/Pods-YouTube-dummy.m \
+  /Users/daniele.bonilha/mob/youtube-iOS/Build/Intermediates/Pods.build/Debug-iphonesimulator/Pods-YouTube.build/module.modulemap
--- Build/Intermediates/Pods.build/Debug-iphonesimulator/Pods-YouTube.build/Objects-normal/x86_64/Pods_YouTube_vers.d
+++ Build/Intermediates/Pods.build/Debug-iphonesimulator/Pods-YouTube.build/Objects-normal/x86_64/Pods_YouTube_vers.d
@@ -1,2 +1,2 @@
 dependencies: \
-  /Users/daniele.bonilha/mob/Quick-Chat/youtube-iOS/Build/Intermediates/Pods.build/Debug-iphonesimulator/Pods-YouTube.build/DerivedSources/Pods_YouTube_vers.c
+  /Users/daniele.bonilha/mob/youtube-iOS/Build/Intermediates/Pods.build/Debug-iphonesimulator/Pods-YouTube.build/DerivedSources/Pods_YouTube_vers.c
